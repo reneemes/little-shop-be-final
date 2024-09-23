95ca608626d549f81da69757a51b9079493032ae
Renee Messersmith <morganrmessersmith@gmail.com>
2024-09-23 16:12:44 -0400

    Fix: got test coverage a little bit higher in the items controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -22,8 +22,8 @@ class Api::V1::ItemsController < ApplicationController
   def update
     item = Item.find(params[:id])
     if !item_params[:merchant_id].nil?
-      merchant = Merchant.find(item_params[:merchant_id]) #needs test coverage
-      render json: ErrorSerializer.format_errors(["Invalid merchant"]), status: :not_found if merchant.nil? #needs test coverage
+      merchant = Merchant.find(item_params[:merchant_id])
+      render json: ErrorSerializer.format_errors(["Invalid merchant"]), status: :not_found if merchant.nil?
     end
     item.update(item_params)
     item.save
